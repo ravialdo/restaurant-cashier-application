1a1a72d769f4cd2da6ff62be3ead88984b9f5588
ravialdo <ravialdo@yahoo.com>
2020-01-22 07:46:26 +0700

    update script

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -14,35 +14,7 @@ class PageController extends Controller
 	
 	public function home()
 	{
-		$data = [
-			'menus' => 	Menu::paginate(6)
-		];
-		
-		alert()->info('Diaplikasi simkas kami!', 'Selamat Datang!')->persistent('close');
-		
-		return view('welcome', $data);
-	}
-	
-	public function order(Request $request, $id)
-	{
-		$menu = Menu::find($id);
-		
-		$customer = Customer::create([
-			'customer_name' => $request->nama_pelanggan,
-			'table_number' => $request->nomor_meja,
-			'gender' => $request->jenis_kelamin,
-			'number_phone' => $request->nomor_handphone,
-			'address' => $request->alamat_lengkap
-		])->get('id')->last();
-		
-		$order = Order::create([
-			'menu_id' => $id,
-			'customer_id' => $customer->id,
-			'amount' => $request->jumlah_makanan,
-			'status' => 'menunggu'
-		]);
-		
-		return redirect('/')->withStatus('Pesanan anda akan segera kami proses, dimohon untuk menunggu sebentar!');
+		return view('auth.login');
 	}
 	
     /**
